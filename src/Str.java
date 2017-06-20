e41a25505b7fbd63c16ba489f1d8c21d6c468736
Nitish280 <nsrccs@gmail.com>
2017-06-20 09:29:15 +0530

    str

---
+++ src/Str.java
@@ -0,0 +1,14 @@
+/* here we create the class */
+public class Str {
+/*here we create the method */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*here we take the two integer value */
+		int a = 5, c=5;
+		/* here we converting the integer value to string */
+		String b = String.valueOf(a);
+		System.out.println(a + b);
+		System.out.println(a + c);
+	}
+
+}
